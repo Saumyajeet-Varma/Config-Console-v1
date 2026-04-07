7b08edff6aec86e286b30ed761a744d24a4eb574
Saumyajeet-Varma <saumyajeetv@gmail.com>
2026-04-07 21:01:29 +0530

    cors setup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors"
 import authRoute from "./routes/auth.route.js";
+
+dotenv.config();
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.UI_URL,
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
